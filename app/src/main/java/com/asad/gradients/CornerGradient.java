33cbe1a853fb5e05fae276339ac66f0f825b234d
asad-201586 <asadul201586@gmail.com>
2020-07-29 02:32:15 +0600

    added corner gradients

---
+++ app/src/main/java/com/asad/gradients/CornerGradient.java
@@ -0,0 +1,14 @@
+package com.asad.gradients;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CornerGradient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_corner_gradient);
+    }
+}
